59a6629edf0a77ef46e5fa9a4630613d48b754bc
alexandregriffet-cmd <alexandre.griffet@gmail.com>
2026-03-14 00:56:07 +0100

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
 window.A4P_CONFIG = {
-  hubHome: 'https://alexandregriffet-cmd.github.io/Diagnostic-mental-A4P-/',
-  cmpApp: 'https://alexandregriffet-cmd.github.io/CMP-A4P-ACADEMIE-DE-PERFORMANCES-/',
-  cmpResults: 'https://alexandregriffet-cmd.github.io/CMP-A4P-ACADEMIE-DE-PERFORMANCES-/resultats.html',
-  pmpApp: '#',
-  psychoApp: '#',
-  hubStorageKey: 'a4p_hub_results'
+  HUB_URL: 'https://alexandregriffet-cmd.github.io/Diagnostic-mental-A4P-/',
+  CMP_URL: 'https://alexandregriffet-cmd.github.io/CMP-A4P-ACADEMIE-DE-PERFORMANCES-/',
+  PMP_URL: '#',
+  EQUILIBRE_URL: '#'
 };
